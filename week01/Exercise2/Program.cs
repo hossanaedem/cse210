3a90fd4971418e3995f4649f7126b958b030669c
hossanaedem <hokon2@byupathway.edu>
2026-07-02 08:21:09 +0100

    Finished Prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
-class Program
+Console.Write("What is your grade percentage? ");
+string? answer = Console.ReadLine();
+int percent = int.Parse(answer ?? "0");
+
+string letter = "";
+
+if (percent >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+    letter = "A";
+}
+else if (percent >= 80)
+{
+    letter = "B";
+}
+else if (percent >= 70)
+{
+    letter = "C";
+}
+else if (percent >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is: {letter}");
+
+if (percent >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the course!");
+}
+else
+{
+    Console.WriteLine("Don't give up! Keep studying and you'll do better next time.!");
 }
