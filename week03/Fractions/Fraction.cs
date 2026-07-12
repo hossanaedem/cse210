eca0977ece253124b472d627cf57cf25be5e95c0
hossanaedem <hokon2@byupathway.edu>
2026-07-12 15:07:13 +0100

    Fraction Commit

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,61 @@
+public class Fraction
+{
+    // Private attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructor 1: Default (1/1)
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor 2: Whole number (top/1)
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    // Constructor 3: Top and Bottom
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    // Setters
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // Returns fraction as a string
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    // Returns decimal value
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
